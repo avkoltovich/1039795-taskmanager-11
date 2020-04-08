8ac8154c340c793910d421cefd568793e70bb8e5
Aleksandr Koltovich <avkoltovich@gmail.com>
2020-04-08 23:12:20 +0300

    Исправляет замечание наставника

--- src/const.js
+++ src/const.js
@@ -1,8 +1,7 @@
-const TASKS_COUNT = 22;
-const SHOWING_TASKS_COUNT_ON_START = 8;
-const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
 const COLORS = [`black`, `yellow`, `blue`, `green`, `pink`];
+
 const DAYS = [`mo`, `tu`, `we`, `th`, `fr`, `sa`, `su`];
+
 const MONTH_NAMES = [
   `January`,
   `February`,
@@ -18,4 +17,4 @@ const MONTH_NAMES = [
   `December`,
 ];
 
-export {TASKS_COUNT, SHOWING_TASKS_COUNT_ON_START, SHOWING_TASKS_COUNT_BY_BUTTON, COLORS, DAYS, MONTH_NAMES};
+export {COLORS, DAYS, MONTH_NAMES};
