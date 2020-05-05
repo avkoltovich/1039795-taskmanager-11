03210d681a750c5f5232ea748f3cd7d30d7e285e
Aleksandr Koltovich <avkoltovich@gmail.com>
2020-05-05 15:11:08 +0300

    refactor: реализует только одну одновременно редактируемую задачу

--- src/components/task-edit.js
+++ src/components/task-edit.js
@@ -191,4 +191,14 @@ export default class TaskEdit extends AbstractSmartComponent {
       });
     }
   }
+
+  reset() {
+    const task = this._task;
+
+    this._isDateShowing = !!task.dueDate;
+    this._isRepeatingTask = Object.values(task.repeatingDays).some(Boolean);
+    this._activeRepeatingDays = Object.assign({}, task.repeatingDays);
+
+    this.rerender();
+  }
 }
