3973517055637aa9cce0d5f0db7561e48fadc1f8
Aleksandr Koltovich <avkoltovich@gmail.com>
2020-05-15 13:39:20 +0300

    refactor: добавляет фильтры в Контроллер, Модель и точку входа

--- src/main.js
+++ src/main.js
@@ -1,23 +1,22 @@
 import BoardComponent from "./components/board.js";
 import BoardController from "./controllers/board.js";
-import FilterComponent from "./components/filter.js";
+import FilterController from "./controllers/filter.js";
 import SiteMenuComponent from "./components/menu.js";
 import TasksModel from "./models/tasks.js";
 import {generateTasks} from "./mocks/task.js";
-import {generateFilters} from "./mocks/filter.js";
 import {render, RenderPosition} from "./utils/render.js";
 
 const TASKS_COUNT = 22;
 
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
+render(siteHeaderElement, new SiteMenuComponent(), RenderPosition.BEFOREEND);
 const randomTasks = generateTasks(TASKS_COUNT);
 const tasksModel = new TasksModel();
 tasksModel.setTasks(randomTasks);
-const randomFilters = generateFilters();
 
-render(siteHeaderElement, new SiteMenuComponent(), RenderPosition.BEFOREEND);
-render(siteMainElement, new FilterComponent(randomFilters), RenderPosition.BEFOREEND);
+const filterController = new FilterController(siteMainElement, tasksModel);
+filterController.render();
 
 const boardComponent = new BoardComponent();
 const boardController = new BoardController(boardComponent, tasksModel);
