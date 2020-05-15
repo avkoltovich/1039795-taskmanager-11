47a49ef92fc0711b27b19fbb7db388a19c6e163e
Aleksandr Koltovich <avkoltovich@gmail.com>
2020-05-15 09:42:20 +0300

    refactor: настраивает Контроллер доски на работу с Моделью

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import BoardComponent from "./components/board.js";
 import BoardController from "./controllers/board.js";
 import FilterComponent from "./components/filter.js";
 import SiteMenuComponent from "./components/menu.js";
+import TasksModel from "./models/tasks.js";
 import {generateTasks} from "./mocks/task.js";
 import {generateFilters} from "./mocks/filter.js";
 import {render, RenderPosition} from "./utils/render.js";
@@ -11,13 +12,15 @@ const TASKS_COUNT = 22;
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 const randomTasks = generateTasks(TASKS_COUNT);
+const tasksModel = new TasksModel();
+tasksModel.setTasks(randomTasks);
 const randomFilters = generateFilters();
 
 render(siteHeaderElement, new SiteMenuComponent(), RenderPosition.BEFOREEND);
 render(siteMainElement, new FilterComponent(randomFilters), RenderPosition.BEFOREEND);
 
 const boardComponent = new BoardComponent();
-const boardController = new BoardController(boardComponent);
+const boardController = new BoardController(boardComponent, tasksModel);
 
 render(siteMainElement, boardComponent, RenderPosition.BEFOREEND);
 boardController.render(randomTasks);
